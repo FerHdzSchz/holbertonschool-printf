eaf43d1049e064f14186ad8f23ad40c516ad7ae1
ferhdzschz <fernandohdz915@hotmail.com>
2022-03-15 03:06:36 -0600

    Modifying main.h to add _printf. We need to fix the bug that makes the last digit dissapear

--- main.h
+++ main.h
@@ -2,7 +2,9 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <unistd.h>
-/*Funcion get_funn_add_buffer*/
+
+/*Add print function*/
+int _printf(const char *format, ...);
 /*All numeric conversions are handled by the functions*/
 /*Decimal, Binary, Octal, Hexadecimal*/
 void pos_num_converter(int number, int base, char *tmp_buff);
