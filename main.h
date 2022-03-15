d46085dba0d281e0812004fbf960c6c108530b48
ferhdzschz <fernandohdz915@hotmail.com>
2022-03-14 23:22:59 -0600

    Adding header file with prototypes of funuctions

---
+++ main.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+
+/*Funcion get_funn_add_buffer*/
+int (*get_print_func(char *s))(int, int)
+/*Funcion para string*/
+int print_str(*char);
+/*Funcion para character*/
+int print_char(*char)
+/*Funcion para int*/
+int print_int(*char)
+/*Funcion para bin*/
+int print_oct(*char)
+/*Funcion para hex*/
+int print_hex(*char)
+/*Funcion para octal*/
+int print_octal(*char)
+/*Funcion para ROT13*/
+char *rot13(char *str)
+/*Funcion strlen*/
+
+/*Funcion Cpstr*/
+
+
